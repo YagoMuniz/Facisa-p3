d338bae2a87e95326284e9ae900878b947e1e79f
yagomuniz <yagomunizns@gmail.com>
2017-09-12 20:55:24 -0300

    Implementado pilha com classe genérica.

---
+++ Part I/EstruturaDeDados/src/Pilha.java
@@ -0,0 +1,52 @@
+
+
+/**
+ * Classe que representa uma pilha genérica de elementos.
+ * @author yagomuniz
+ * */
+public class Pilha<T> {
+
+    private static final int TAMANHO_INICIAL = 3;
+    private int inseridos;
+    private T elementos[];
+
+    public Pilha(){
+        this.elementos = (T[])new Object[TAMANHO_INICIAL];
+    }
+
+    public void push(T elemento){
+
+        if(this.elementos.length == inseridos){
+            T arrayAuxiliar[] = (T[])new Object[this.elementos.length*2];
+
+            for (int i = 0; i < this.elementos.length; i++) {
+                arrayAuxiliar[i] = this.elementos[i];
+            }
+            this.elementos = arrayAuxiliar;
+        }
+
+        this.elementos[inseridos] = elemento;
+        this.inseridos++;
+    }
+
+    public T pop(){
+        T topo = top();
+        if(topo != null){
+            this.elementos[inseridos-1] = null;
+            this.inseridos--;
+        }
+        return topo;
+    }
+
+    public T top(){
+        return isEmpty() ? null : this.elementos[inseridos-1];
+    }
+
+    public int tamanho(){
+        return inseridos;
+    }
+
+    public boolean isEmpty(){
+        return tamanho() == 0;
+    }
+}
---
+++ Part I/EstruturaDeDados/src/PilhaTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PilhaTest {
+
+    @Test
+    public void checkIsEmpty(){
+        Pilha<String> pilha = new Pilha<String>();
+        Assert.assertTrue(pilha.isEmpty());
+    }
+
+    @Test
+    public void checkIsEmptyFail(){
+        Pilha<String> pilha = new Pilha<String>();
+        pilha.push("A");
+        Assert.assertFalse(pilha.isEmpty());
+    }
+
+    @Test
+    public void checkSize(){
+        Pilha<String> pilha = new Pilha<String>();
+        pilha.push("A");
+        pilha.push("B");
+        pilha.push("C");
+
+        Assert.assertEquals(3, pilha.tamanho());
+    }
+
+    @Test
+    public void checkTop(){
+        Pilha<String> pilha = new Pilha<String>();
+        pilha.push("A");
+        pilha.push("B");
+        pilha.push("C");
+
+        Assert.assertEquals("C", pilha.top());
+    }
+}
